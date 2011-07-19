57d14075bfb60bd5bb124bcf68d8611e78b8d3d8
Jörn Kottmann <joern@apache.org>
2011-07-19 17:36:12 +0000

    OPENNLP-235 Added preference initializer

---
+++ opennlp-caseditor-plugin/src/main/java/org/apache/opennlp/caseditor/OpenNLPPreferenceConstants.java
@@ -0,0 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.opennlp.caseditor;
+
+public class OpenNLPPreferenceConstants {
+
+  public static final String MODEL_PATH = "MODEL_PATH";
+  
+  public static final String SENTENCE_TYPE = "SENTENCE_TYPE";
+  public static final String TOKEN_TYPE = "TOKEN_TYPE";
+  public static final String NAME_TYPE = "NAME_TYPE";
+}
---
+++ opennlp-caseditor-plugin/src/main/java/org/apache/opennlp/caseditor/OpenNLPPreferenceInitializer.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.opennlp.caseditor;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+public class OpenNLPPreferenceInitializer extends AbstractPreferenceInitializer {
+
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = OpenNLPPlugin.getDefault().getPreferenceStore();
+		
+		// For now lets hard code configuration here!
+		store.setDefault(OpenNLPPreferenceConstants.SENTENCE_TYPE, "org.apache.opennlp.annotations.Sentence");
+		store.setDefault(OpenNLPPreferenceConstants.NAME_TYPE, "org.apache.opennlp.annotations.Person");
+	}
+}
