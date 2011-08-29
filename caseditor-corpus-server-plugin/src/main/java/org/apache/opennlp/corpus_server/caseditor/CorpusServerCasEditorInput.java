85d7f0e7ad17530e9558c61a9147a084fe7c152e
Jörn Kottmann <joern@apache.org>
2011-08-29 17:22:31 +0000

    OPENNLP-252 Added corpus server editor input, there a resource is identified by an URL and cas id

---
+++ caseditor-corpus-server-plugin/src/main/java/org/apache/opennlp/corpus_server/caseditor/CorpusServerCasEditorInput.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.opennlp.corpus_server.caseditor;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IPersistableElement;
+
+// Serialization must be supported, right?!
+public class CorpusServerCasEditorInput implements IEditorInput {
+
+  private String corpusUrl;
+  private String casId;
+  
+  CorpusServerCasEditorInput(String corpusUrl, String casId) {
+    this.corpusUrl = corpusUrl;
+    this.casId = casId;
+  }
+  
+  @Override
+  public Object getAdapter(Class clazz) {
+    return null;
+  }
+
+  // can be checked through an http lookup
+  @Override
+  public boolean exists() {
+    return false;
+  }
+
+  @Override
+  public ImageDescriptor getImageDescriptor() {
+    return null;
+  }
+
+  @Override
+  public String getName() {
+    return casId;
+  }
+
+  // it is ... 
+  @Override
+  public IPersistableElement getPersistable() {
+    return null;
+  }
+
+  @Override
+  public String getToolTipText() {
+    return "";
+  }
+
+  public String getServerUrl() {
+    return corpusUrl;
+  }
+}
