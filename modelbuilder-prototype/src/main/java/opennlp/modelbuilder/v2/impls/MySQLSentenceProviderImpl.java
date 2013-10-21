6ea85367c898e7aa353471ab67b0b5e857ad4dd4
Mark Giaconia <markg@apache.org>
2013-10-21 23:13:57 +0000

    OPENNLP-607 Changed header template. Referenced new jira ticket

--- modelbuilder-prototype/src/main/java/opennlp/modelbuilder/v2/impls/MySQLSentenceProviderImpl.java
+++ modelbuilder-prototype/src/main/java/opennlp/modelbuilder/v2/impls/MySQLSentenceProviderImpl.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package opennlp.modelbuilder.v2.impls;
 
@@ -60,9 +71,9 @@ public class MySQLSentenceProviderImpl implements SentenceProvider {
   private static Connection getMySqlConnection() throws Exception {
     // EntityLinkerProperties property = new EntityLinkerProperties(new File("c:\\temp\\opennlpmodels\\entitylinker.properties"));
     String driver = "org.gjt.mm.mysql.Driver";
-    String url = "jdbc:mysql://127.0.0.1:3306/wink";
+    String url = "jdbc:mysql://localhost:3306/db";
     String username = "root";
-    String password = "559447";
+    String password = "??";
 
     Class.forName(driver);
     Connection conn = DriverManager.getConnection(url, username, password);
