8ed861d8319528010678625381afbd118b85522a
Mark Giaconia <markg@apache.org>
2013-11-12 12:00:59 +0000

    OPENNLP-615 Cleaned up javadocs and header info in ModelBasedScorer

--- apache-opennlp-addons/src/main/java/org/apache/opennlp/addons/tools/entitylinker/geoentitylinker/ModelBasedScorer.java
+++ apache-opennlp-addons/src/main/java/org/apache/opennlp/addons/tools/entitylinker/geoentitylinker/ModelBasedScorer.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.opennlp.addons.tools.entitylinker.geoentitylinker;
 
@@ -35,7 +46,7 @@ import opennlp.tools.util.Span;
 
 /**
  *
- * @author Owner
+ *Utilizes a doccat model to score toponyms based on surrounding context
  */
 public class ModelBasedScorer implements LinkedEntityScorer<CountryContext> {
 
